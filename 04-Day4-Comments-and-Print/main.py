1444c1600bab3c829f54292ac8c5073117325a54
Shubhendu <shubhendu.prasad0901@gmail.com>
2024-12-31 12:02:28 +0530

    Day 4

---
+++ 04-Day4-Comments-and-Print/main.py
@@ -0,0 +1,10 @@
+# #hey Hari, Please do not remove this line
+# Author: Hari
+# Course: 100 Days of Python
+print("Hey I am good boy \nand the viewer is also a good boy")
+"""
+print("Hey I am good boy \nand the viewer is also a good boy")
+print("\nthe viewer is also a good boy")
+print("\nHey I am good boy")""" #multi-line comment
+
+print("Hey",6,7,sep="~", end="*")
